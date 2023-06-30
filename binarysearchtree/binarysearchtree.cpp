0cf381f61c443c5c6ffe6d59ac1250e6776bc542
tiarahasnabila <tiarahasnabila@gmail.com>
2023-06-30 22:34:22 +0700

    jika parent null

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -58,4 +58,17 @@ public:
 
 	void search(string element, Node*& parent, Node*& currentNode)
 	{
-		
+		// This function searches the currentNode of the specified Node as well as the currentNode of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
+	
